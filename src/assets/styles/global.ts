0bde663865287b6e7760e9a6255194be45bd71e0
Isabela Hyeda <isabelahyeda@gmail.com>
2023-05-31 19:31:07 -0300

    build: setup inicial do projeto

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "sans-serif"
+    }
+
+    body {
+        background-color: pink;
+        text-align: center;
+    }
+`
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import Login from './pages/Login/index.tsx'
+import Register from './pages/Register/index.tsx'
+import Home from './pages/Home/index.tsx'
+import GlobalStyles from './assets/styles/global'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+    ]
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+    <GlobalStyles />
+  </React.StrictMode>
 )
